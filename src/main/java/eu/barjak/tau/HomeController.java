cd43abd251db22a2dd0cf7130bab4c656aa96f16
lbarjak <barjak.laszlo@gmail.com>
2023-03-25 23:08:14 +0100

    @RequestMapping instead of @GetMapping & @PostMapping

--- src/main/java/eu/barjak/tau/HomeController.java
+++ src/main/java/eu/barjak/tau/HomeController.java
@@ -3,9 +3,9 @@ package eu.barjak.tau;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomeController {
@@ -15,14 +15,7 @@ public class HomeController {
     @Autowired
     private WeatherService weatherService;
 
-    @GetMapping("/")
-    public String neu(Model model) {
-        model.addAttribute("data", data);
-        weatherService.weather(data);
-        return "index";
-    }
-
-    @PostMapping("/")
+    @RequestMapping(value = "/", method = { RequestMethod.GET, RequestMethod.POST })
     public String dataForm(@ModelAttribute(value = "data") Data data, Model model) {
         model.addAttribute("data", data);
         weatherService.weather(data);
