a9470db75cbc4a504667d7b33a0fd935556e37bb
lbarjak <barjak.laszlo@gmail.com>
2023-03-27 17:57:34 +0200

    lombok added

--- src/main/java/eu/barjak/tau/Data.java
+++ src/main/java/eu/barjak/tau/Data.java
@@ -3,6 +3,11 @@ package eu.barjak.tau;
 import java.util.ArrayList;
 import java.util.List;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class Data {
 
     private int thermalTimeConstant = 50;// min. 3.3 max. 122
@@ -15,77 +20,5 @@ public class Data {
 
     private List<Temperature> temperatures = new ArrayList<>();
     private int indexOfMeasuredTemperatures;
-
-    public int getCorrection() {
-        return this.correction;
-    }
-
-    public void setCorrection(int correction) {
-        this.correction = correction;
-    }
-
-    public int getThermalTimeConstant() {
-        return this.thermalTimeConstant;
-    }
-
-    public void setThermalTimeConstant(int thermalTimeConstant) {
-        this.thermalTimeConstant = thermalTimeConstant;
-    }
-
-    public String getStartDate() {
-        return this.startDate;
-    }
-
-    public void setStartDate(String startDate) {
-        this.startDate = startDate;
-    }
-
-    public String getEndDate() {
-        return this.endDate;
-    }
-
-    public void setEndDate(String endDate) {
-        this.endDate = endDate;
-    }
-
-    public String getStartTime() {
-        return this.startTime;
-    }
-
-    public void setStartTime(String startTime) {
-        this.startTime = startTime;
-    }
-
-    public Double getInitRoomTemp() {
-        return this.initRoomTemp;
-    }
-
-    public void setInitRoomTemp(Double initRoomTemp) {
-        this.initRoomTemp = initRoomTemp;
-    }
-
-    public int getOmszId() {
-        return this.omszId;
-    }
-
-    public void setOmszId(int omszId) {
-        this.omszId = omszId;
-    }
-
-    public List<Temperature> getTemperatures() {
-        return this.temperatures;
-    }
-
-    public void setTemperatures(List<Temperature> temperatures) {
-        this.temperatures = temperatures;
-    }
-
-    public int getIndexOfMeasuredTemperatures() {
-        return this.indexOfMeasuredTemperatures;
-    }
-
-    public void setIndexOfMeasuredTemperatures(int indexOfMeasuredTemperatures) {
-        this.indexOfMeasuredTemperatures = indexOfMeasuredTemperatures;
-    }
 
 }
--- src/main/java/eu/barjak/tau/Temperature.java
+++ src/main/java/eu/barjak/tau/Temperature.java
@@ -1,5 +1,10 @@
 package eu.barjak.tau;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class Temperature {
 
     private String date;
@@ -8,43 +13,4 @@ public class Temperature {
     private Double outdoorTemp;
     private Double roomTemp;
 
-    public String getDate() {
-        return date;
-    }
-
-    public void setDate(String date) {
-        this.date = date;
-    }
-
-    public String getTime() {
-        return time;
-    }
-
-    public void setTime(String time) {
-        this.time = time;
-    }
-
-    public String getDay() {
-        return day;
-    }
-
-    public void setDay(String day) {
-        this.day = day;
-    }
-
-    public Double getOutdoorTemp() {
-        return outdoorTemp;
-    }
-
-    public void setOutdoorTemp(Double outdoorTemp) {
-        this.outdoorTemp = outdoorTemp;
-    }
-
-    public Double getRoomTemp() {
-        return roomTemp;
-    }
-
-    public void setRoomTemp(Double roomTemp) {
-        this.roomTemp = roomTemp;
-    }
 }
