7309b959dc6e6d18d982cb0a52b9011906f4315c
Árpád Magosányi <m4gw4s@gmail.com>
2023-03-27 09:36:10 +0200

    Update HomeController.java

--- src/main/java/eu/barjak/tau/HomeController.java
+++ src/main/java/eu/barjak/tau/HomeController.java
@@ -24,6 +24,7 @@ public class HomeController {
 
     @GetMapping("/")
     public String neu(Model model) {
+        //debug infó benntmaradt
         System.out.println("in GET: " + data.getThermalTimeConstant());
         weatherService.weather();
         extracted(model);
@@ -32,9 +33,12 @@ public class HomeController {
 
     @PostMapping("/")
     public String dataForm(@ModelAttribute(value = "data") Data data, Model model) {
+        // nem látok input validációt
         extracted(model);
+        //debug infó benntmaradt
         System.out.println("in POST1: " + data.getThermalTimeConstant());
         weatherService.weather();
+        //debug infó benntmaradt
         System.out.println("in POST2: " + data.getThermalTimeConstant());
         return "index";
     }
